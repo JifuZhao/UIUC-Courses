fc9adb451dcc73e45976637b147f8c0cdc1959f0
JifuZhao <jzhao59@illinois.edu>
2016-09-26 22:32:02 -0500

    edit codes

--- UIUC_ECE544/HW2/code/NeuralNetwork.py
+++ UIUC_ECE544/HW2/code/NeuralNetwork.py
@@ -16,7 +16,7 @@ import time
 
 class NeuralNetwork(object):
     """ self-defined Neural Network classifier """
-    
+
     def __init__(self, netSize, loss, maxIter=500, batchSize=100,
                  learningRate=0.1, CV=False):
         self.netSize = netSize
@@ -38,26 +38,25 @@ class NeuralNetwork(object):
         # randomly initialize the weight matrix w
         numNode = [len(X[0])] + list(self.netSize)
         for i in range(self.layer - 1):
-            if self.loss == 'relu':
-                randomW = (np.random.random((numNode[i + 1], numNode[i] + 1)) - 0.5)
-            else:
-                randomW = (np.random.random((numNode[i + 1], numNode[i] + 1)) - 0.5)
+            randomW = (np.random.random((numNode[i + 1], numNode[i] + 1)) - 0.5)
+            # if self.loss == 'relu':
+            #     randomW = (np.random.random((numNode[i + 1], numNode[i] + 1)) - 0.5)
+            # else:
+            #     randomW = (np.random.random((numNode[i + 1], numNode[i] + 1)) - 0.5)
             self.w.append(randomW)
 
         # begin training process
         for iterate in range(1, self.maxIter + 1):
             X, y = shuffle(X, y)
             batchesX, batchesY = self.getBatches(X, y)
-            
+
             for batchFeature, BatchLabel in zip(batchesX, batchesY):
                 self.w = self.updateW(batchFeature, BatchLabel, self.w)
-                
+
             self.trainAcc.append(self.evaluate(X, y, self.w))
             if (iterate % showFreq == 0):
                 print(iterate, "th Iteration is done, used time\t",
                       np.round(time.time() - t0, 2), 's')
-                
-            # print("w and acc\t", self.w[0][:3, :3], self.trainAcc[-1])
 
         print("Reach the maximum iteration, training is done !")
         print("Total training time: \t", np.round(time.time() - t0, 2), 's')
@@ -68,24 +67,38 @@ class NeuralNetwork(object):
         n = len(X)
         g = [self.addBias(X)]  # g means x, h(w'x)
         z = [X]  # z means x, w'x, w'g(w'x)
-        for i in range(self.layer - 1):
+
+        # for i in range(self.layer - 1):
+        #     forwardW = w[i]
+        #     feature = g[i]
+        #     In = np.dot(feature, forwardW.T)
+        #     z.append(In)
+        #     Out = self.nonLinearity(In)
+        #     if i != self.layer - 2:
+        #         g.append(self.addBias(Out))
+        #     else:
+        #         g.append(Out)
+
+        for i in range(self.layer - 2):
             forwardW = w[i]
             feature = g[i]
             In = np.dot(feature, forwardW.T)
             z.append(In)
             Out = self.nonLinearity(In)
-            if i != self.layer - 2:
-                g.append(self.addBias(Out))
-            else:
-                g.append(Out)
+            g.append(self.addBias(Out))
+
+        forwardW = w[self.layer - 2]
+        feature = g[self.layer - 2]
+        In = np.dot(feature, forwardW.T)
+        z.append(In)
+        Out = self.softmax(In)
+        g.append(Out)
 
         gradient = self.computeGradient(g, z, w, y)
-      
+
         newW = []
         for i in range(self.layer - 1):
             newW.append(w[i] - self.learningRate * gradient[i] / n)
-            
-            # print("gradient\t", (self.learningRate * gradient[i] / n)[:3, :3])
 
         return newW
 
@@ -93,7 +106,11 @@ class NeuralNetwork(object):
     def computeGradient(self, g, z, w, y):
         """ function to compute the gradient """
         gradient = []
-        b = (g[-1] - y) * self.backGradient(z[-1])
+        # b = (g[-1] - y) * self.backSoftmax(z[-1])
+        label = np.argmax(y, axis=1)
+        b = g[-1]
+        b[range(len(b)), label] -= 1
+
         for i in reversed(range(self.layer - 1)):
             gradient.append(np.dot(b.T, g[i]))
             b = (np.dot(b, w[i])[:, 1:]) * self.backGradient(z[i])
@@ -125,14 +142,21 @@ class NeuralNetwork(object):
         """ function to predict X based on trained model """
         feature = X
         # print("feature at ", 0, ' layer', feature[:3, :])
+
+        # for i in range(self.layer - 1):
+        #     weight = w[i]
+        #     feature = self.addBias(feature)
+        #     feature = self.nonLinearity(np.dot(feature, weight.T))
         
-        for i in range(self.layer - 1):
+        for i in range(self.layer - 2):
             weight = w[i]
             feature = self.addBias(feature)
             feature = self.nonLinearity(np.dot(feature, weight.T))
-            
-            # print("feature at ", i + 1, ' layer', feature[:3, :3])
-        
+
+        weight = w[self.layer - 2]
+        feature = self.addBias(feature)
+        feature = self.softmax(np.dot(feature, weight.T))
+
         prediction = np.argmax(feature, axis=1)
         return prediction
 
@@ -164,6 +188,14 @@ class NeuralNetwork(object):
         return g
 
 
+    def softmax(self, z):
+        """ function to calculate the softmax function """
+        ans = np.exp(z)
+        ans = ans.T / np.sum(ans, axis=1)
+        ans = ans.T
+        return ans
+
+
     def getBatches(self, X, y):
         """ function to get the batches """
         left = len(X) % self.batchSize
@@ -183,7 +215,7 @@ class NeuralNetwork(object):
         """ function to add bias item to the data as the first column """
         n = len(data)
         if self.loss == 'relu':
-            data = np.append(np.array([np.ones(n)]).T / 10, data, axis=1)
+            data = np.append(np.array([np.ones(n)]).T, data, axis=1)
         else:
             data = np.append(np.array([np.ones(n)]).T, data, axis=1)
         return data
--- UIUC_IE529/hw2/code/PCA.py
+++ UIUC_IE529/hw2/code/PCA.py
@@ -6,7 +6,61 @@ __email__       = "jzhao59@illinois.edu"
 __date__        = "09/24/2016"
 """
 
+import warnings
+warnings.simplefilter('ignore')
+
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class PCA(object):
+    """ Class to perform PCA on given input X """
+    def __init__(self, n=None, whiten=False):
+        self.n = n
+        self.whiten = False
+        self.eigenvector = None
+        self.variance = None
+        self.mean = None
+
+
+    def fit(self, X):
+        """ function to conduct PCA """
+        centeredX = self.center(X)
+        covariance = np.dot(centeredX.T, centeredX)
+        [U, S, V] = np.linalg.svd(covariance)
+        self.eigenvector = U
+        self.variance = S
+
+
+    def center(self, X):
+        """ function to center the input X through substracting the mean value """
+        self.mean = np.mean(X, axis=0)
+        centeredX = X - self.mean
+        if self.whiten == True:  # if whiten == True, divide X by standard deviation
+            std = np.std(centeredX)
+            centeredX = centeredX / std
+
+        return centeredX
+
+
+    def transform(self, X):
+        """ function to project X onto the first n principal components """
+        if self.n is None:
+            result = np.dot(X, self.eigenvector)
+        else:
+            result = np.dot(X, self.eigenvector[:, :self.n])
+
+        return result
+
+
+    def getResult(self):
+        """ function to get the first n principal components and n variance """
+        if self.n is None:
+            print('Keep the all ' + str(len(self.variance)) + ' principal components')
+            return self.eigenvector, self.variance, self.mean
+        else:
+            print('Keep the first ' + str(self.n) + ' principal components')
+            return self.eigenvector[:, :self.n], self.variance[:self.n], self.mean
 
 class PCA(object):
     """ class PCA is used to perform principal component analysis """
@@ -25,20 +79,28 @@ class PCA(object):
         self.mean = np.mean(X, axis=0)
         centeredX = X - self.mean
         U, S, V = np.linalg.svd(centeredX)
-        self.variance = S ** 2
-        self.component = V
+        self.variance = (S ** 2) / len(X)
+        self.component = V.T
 
         return self.variance[:self.n], self.mean, self.component[:, :self.n]
 
 
 def main():
-    X = np.random.random((10, 4))
-    pca = PCA(n=4)
-    variance, mean, eigenvector = pca.fit(X)
-    print('Variance: \t', variance)
-    print('Mean: \t', mean)
-    print('Eigenvector: \t', eigenvector)
+    data = pd.read_csv('./PCAdata.csv', header=None).values.T
+    pca = PCA(2)
+    variance, mean, component = pca.fit(data)
+    print(variance)
+    print(mean)
+    print(component)
 
+    project = np.dot(data - mean, component)
+
+    fig, ax = plt.subplots()
+    ax.plot(project[:, 0], project[:, 1], 'g.')
+    ax.set_title('PCA projection')
+    ax.set_xlabel('First principal component')
+    ax.set_ylabel('Second principal component')
+    plt.show()
 
 if __name__ == '__main__':
     main()
