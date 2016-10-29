cbc5a321c6d3bb948f0ec2871509e43c9f503cd2
JifuZhao <jzhao59@illinois.edu>
2016-10-28 23:21:53 -0500

    edit hw4 for ECE544

---
+++ UIUC_ECE544/hw4/code/RBM.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+__author__      = "Jifu Zhao"
+__email__       = "jzhao59@illinois.edu"
+__date__        = "10/28/2016"
+"""
+
+import warnings
+warnings.simplefilter('ignore')
+
+import numpy as np
+
+class RBM(object):
+    """ self-defined class for Restricted Boltzmann Machine (RBM)"""
+
+    def __init__():
+        pass
+
+
+    def train():
+        pass
+
+
+    def update_h():
+        pass
+
+    def update_v():
+        pass
+
+    def compute_likelihood():
+        pass
+
+    def get_params():
+        pass
